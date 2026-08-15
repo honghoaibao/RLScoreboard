a49fe9346c05dbe08e6985ae6c789a44d5296a68
honghoaibao <honghoaibaoat@gmail.com>
2026-08-15 02:03:05 +0000

    ol

--- src/main/kotlin/dev/rlscoreboard/integration/Integration.kt
+++
@@ -1,8 +0,0 @@
-package dev.rlscoreboard.integration
-
-/** One optional third-party integration. Never depended on directly by core engines - see section 4/22. */
-interface Integration {
-    val id: String
-    fun isAvailable(): Boolean
-    fun enable()
-}
